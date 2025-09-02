cc26124dd41c222d90fd295c182deed1cb1b1ac4
academicschoolbd <159193937+academicschoolbd@users.noreply.github.com>
2025-09-02 13:39:04 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,26 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => self.clients.claim());
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request));
+const CACHE_NAME = 'amar-bangla-app-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/about.html',
+  '/contact.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
 });
